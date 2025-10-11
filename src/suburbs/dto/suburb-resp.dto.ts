bb8e97f00de1ed9ba767bb11584e37d62c01b542
Pulkit Mehta <mehta.pulkit94@gmail.com>
2025-10-11 13:00:22 +1100

    Minor update to suburb api

---
+++ src/suburbs/dto/suburb-resp.dto.ts
@@ -0,0 +1,10 @@
+// suburbs/dto/suburb-response.dto.ts
+import { Exclude, Expose } from 'class-transformer';
+
+@Exclude()
+export class SuburbResponseDto {
+    @Expose() id: string;
+    @Expose() name: string;
+    @Expose() lga_name?: string;
+    @Expose() area_ha?: number;
+}
