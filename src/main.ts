c3d7ea4e0298762fc5df1ade9f291c1a1d0db2d3
Pulkit Mehta <mehta.pulkit94@gmail.com>
2025-10-11 13:15:56 +1100

    CORS for prod

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',          // local dev
-      'https://your-frontend.vercel.app', // production
+      'https://www.thriveee.com.au', // production
     ],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
