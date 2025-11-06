881d5b198afe2530b33cfe82fa73bb99e52176c7
Pulkit Mehta <mehta.pulkit94@gmail.com>
2025-11-06 22:38:29 +1100

    port updated

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   });
 
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
   console.log(`🚀 Server running on http://localhost:3000`);
 }
 bootstrap();
