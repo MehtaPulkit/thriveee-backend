354980b46751a5a83d3ab28f6424c38c5f063c78
Pulkit Mehta <mehta.pulkit94@gmail.com>
2026-02-16 14:18:54 +1100

    src updated

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -7,7 +7,7 @@ import { Service } from '../services/services.entity';
 import { Provider } from '../providers/provider.entity';
 import { Order } from '../orders/order.entity';
 import { BookingOrchestratorService } from './booking-orchestrator.service';
-import { PricingModule } from 'src/pricing/pricing.module';
+import { PricingModule } from '../pricing/pricing.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Booking, Service, Provider, Order]), PricingModule],
